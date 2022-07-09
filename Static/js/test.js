39ad6fac5e37e7b2c5ff3afc59e190d35c9fae18
hanniecodes <hannahgallen@gmail.com>
2022-07-09 16:23:54 -0400

    New map data

---
+++ Static/js/test.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+
+// Reading a file
+
+// let rawData = fs.readFileSync('test.json')
+// let covidData = JSON.parse(rawData)
+
+// console.log(covidData)
+
+fs.readFile('test.json', (err, data) => {
+    if (err) throw err;
+    let covidData = JSON.parse(data)
+    // console.log(covidData)
+
+    // Using map to modify data
+    let plotData = covidData.map(element => {
+        return {id: `US-${element.state}`, value: element.positive, month: element.YearMonth.month, year: element.YearMonth.qyear }
+        // return {id: element.state, value: element.death}
+    })
+    console.log(plotData)
+})
