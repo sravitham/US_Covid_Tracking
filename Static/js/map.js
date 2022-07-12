b504d41b5cfbff06a50fc3e1b78ccca87af4df7e
hanniecodes <hannahgallen@gmail.com>
2022-07-12 10:13:47 -0400

    ha 7/12

--- Static/js/map.js
+++ Static/js/map.js
@@ -1,40 +1,28 @@
-// Kelechi code
-// fs.readFile('../../Data/Covid_slider_all.json', (err, data) => {
-//   if (err) throw err;
-//   let covidData = JSON.parse(data)
-//   // console.log(covidData)
 
-//   // Using map to modify data
-//   let plotData = covidData.map(element => {
-//       return {id: `US-${element.state}`, value: element.positive, month: element.YearMonth.month, year: element.YearMonth.qyear }
-//       // return {id: element.state, value: element.death}
-//   })
-//   console.log(plotData)
-// });
+// COntingency map
 
 // Map function
 function createMap(){
   // states= ["AL","FL","WA"]
   chosenFile= '../../Data/allstatesAG_map.json'
   d3.json(chosenFile).then((data) => {
+    // const covidData= JSON.parse(data);
     console.log(data);
     covidSeries=[];
-    var plotData = data.forEach(element => {
+
+    data.forEach(element => {
       states = `US-${element.state}`;
       // console.log(states);
       values=element.positive;
       // console.log(values);
-      console.log(plotData);
+      covidSeries.push({ id:states, 
+          value: values });
       
-      covidSeries.push({ state: plotData.states, 
-          value: plotData.values });
-      // console.log(covidSeries);
-      return covidSeries;
     });
+    console.log(covidSeries);
     
     
-    
-    });
+
 
 
 
@@ -93,27 +81,65 @@ function createMap(){
     });
   
   
-    // covidSeries=[];
-    // states.forEach(state => {
-    //   // // checking to see if obj is undefined
-    //   console.log(state in data['positive'])
-    //   if (state in data['positive']){
-    //     // covidSeries.push({ date: data['date'][state], id: state, value: data['positive'][state]});
-    //     covidSeries.push({
-    //     year: sta.YearMonth.qyear ,
-    //     id: state,
-    //     // id:"US-WA",
-    //     // id: "\"US-"+ state + "\"" , 
-    //     //  id: `US-${state}`, 
-    //     value: data['positive'][state]});
-    //     // covidSeries.replace(/'/g, '"');
-    //     // covidSeries.replaceAll("'", '"')
-    //   };
-      
-    //   console.log(covidSeries)
-    // });
 
-    // polygonSeries.data.setAll([covidSeries]);
+    polygonSeries.data.setAll([
+    {id:  'US-AK', value:  56886},
+     {id:  'US-AL', value:  499819},
+     {id:  'US-AR', value:  324818},
+     {id:  'US-AS', value:  0},
+     {id:  'US-AZ', value:  826454},
+     {id:  'US-CA', value:  3501394},
+     {id:  'US-CO', value:  436602},
+     {id:  'US-CT', value:  285330},
+     {id:  'US-DC', value:  41419},
+     {id:  'US-DE', value:  88354},
+     {id:  'US-FL', value:  1909209},
+     {id:  'US-GA', value:  1023487},
+     {id:  'US-GU', value:  7749},
+     {id:  'US-HI', value:  28699},
+     {id:  'US-IA', value:  282384},
+     {id:  'US-ID', value:  172931},
+     {id:  'US-IL', value:  1198335},
+     {id:  'US-IN', value:  667262},
+     {id:  'US-KS', value:  295861},
+     {id:  'US-KY', value:  410709},
+     {id:  'US-LA', value:  433785},
+     {id:  'US-MA', value:  591356},
+     {id:  'US-MD', value:  387319},
+     {id:  'US-ME', value:  45794},
+     {id:  'US-MI', value:  656072},
+     {id:  'US-MN', value:  490011},
+     {id:  'US-MO', value:  480643},
+     {id:  'US-MP', value:  145},
+     {id:  'US-MS', value:  297581},
+     {id:  'US-MT', value:  100914},
+     {id:  'US-NC', value:  872176},
+     {id:  'US-ND', value:  100391},
+     {id:  'US-NE', value:  203026},
+     {id:  'US-NH', value:  76861},
+     {id:  'US-NJ', value:  812609},
+     {id:  'US-NM', value:  186922},
+     {id:  'US-NV', value:  296190},
+     {id:  'US-NY', value:  1681169},
+     {id:  'US-OH', value:  978471},
+     {id:  'US-OK', value:  428997},
+     {id:  'US-OR', value:  157079},
+     {id:  'US-PA', value:  948643},
+     {id:  'US-PR', value:  101632},
+     {id:  'US-RI', value:  128781},
+     {id:  'US-SC', value:  525865},
+     {id:  'US-SD', value:  113589},
+     {id:  'US-TN', value:  782206},
+     {id:  'US-TX', value:  2686818},
+     {id:  'US-UT', value:  374850},
+     {id:  'US-VA', value:  585700},
+     {id:  'US-VI', value:  2714},
+     {id:  'US-VT', value:  16083},
+     {id:  'US-WA', value:  344532},
+     {id:  'US-WI', value:  621654},
+     {id:  'US-WV', value:  133445},
+     {id:  'US-WY', value:  54764},
+    ]);
     
     var heatLegend = chart.children.push(am5.HeatLegend.new(root, {
       orientation: "vertical",
@@ -139,102 +165,7 @@ function createMap(){
       heatLegend.set("startValue", polygonSeries.getPrivate("valueLow"));
       heatLegend.set("endValue", polygonSeries.getPrivate("valueHigh"));
     });
-  };
-  // Make stuff animate on load ALL THE CODE BELOW IS THE SLIDER
-// chart.appear(1000, 100);
-
-// // Aggregate data
-// var years = {};
-// var firstYear = 99999;
-// var lastYear = 0;
-// for(var i = 0; i < data.length; i++) {
-//   var row = data[i];
-//   var year = row.year;
-//   if (years[year] == undefined) {
-//     years[year] = [];
-//   }
-//   years[year].push(row.state);
-  
-//   if (firstYear > year) {
-//     firstYear = year;
-//   }
-//   if (lastYear < year) {
-//     lastYear = year;
-//   }
-// }
-
-// // Create controls 
-// var container = chart.children.push(am5.Container.new(root, {
-//   y: am5.p100,
-//   centerX: am5.p50,
-//   centerY: am5.p100,
-//   x: am5.p50,
-//   width: am5.percent(90),
-//   layout: root.horizontalLayout,
-//   paddingBottom: 10
-// }));
-
-// var playButton = container.children.push(am5.Button.new(root, {
-//   themeTags: ["play"],
-//   centerY: am5.p50,
-//   marginRight: 40,
-//   icon: am5.Graphics.new(root, {
-//     themeTags: ["icon"]
-//   })
-// }));
-
-// playButton.events.on("click", function () {
-//   if (playButton.get("active")) {
-//     slider.set("start", slider.get("start") + 0.0001);
-//   } else {
-//     slider.animate({
-//       key: "start",
-//       to: 1,
-//       duration: 15000 * (1 - slider.get("start"))
-//     });
-//   }
-// });
-
-// var slider = container.children.push(am5.Slider.new(root, {
-//   //width: am5.percent(80),
-//   orientation: "horizontal",
-//   start: 0,
-//   centerY: am5.p50
-// }));
-
-// slider.startGrip.get("icon").set("forceHidden", true);
-// slider.startGrip.set("label", am5.Label.new(root, {
-//   text: firstYear + "",
-//   paddingTop: 0,
-//   paddingRight: 0,
-//   paddingBottom: 0,
-//   paddingLeft: 0
-// }));
-
-
-// updateState(firstYear);
-
-// slider.events.on("rangechanged", function () {
-//   var year = firstYear + Math.round(slider.get("start", 0) * (lastYear - firstYear));
-//   slider.startGrip.get("label").set("text", year + "");
-//   updateState(year);
-//   console.log(year)
-//   // updateSeriesData(
-//   //   firstYear + Math.round(slider.get("start", 0) * (lastYear - firstYear))
-//   // );
-// });
-
-// function updateState(year) {
-//   am5.object.each(years, function(joinYear, State) {
-//     //console.log(State)
-//     am5.array.each(State, function(country) {
-//       var dataItem = polygonSeries.getDataItemById(country);
-//       if (dataItem) {
-//         dataItem.get("mapPolygon").set("active", joinYear <= year)
-//       }
-//     })
-//   })
-// }
-
-// };
+  });
+ 
+};
 createMap();
