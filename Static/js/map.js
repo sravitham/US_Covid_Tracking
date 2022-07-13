e40e726e3b81fa7a55ca1e2a14b138875049fe1e
hanniecodes <hannahgallen@gmail.com>
2022-07-13 19:58:45 -0400

    chloro map

--- Static/js/map.js
+++ Static/js/map.js
@@ -76,8 +76,11 @@ function createMap(){
       key: "fill"
     }]);
 
-    polygonSeries.mapPolygons.template.events.on("pointerover", function(ev) {
+    polygonSeries.mapPolygons.template.events.on("click", function(ev) {
       heatLegend.showValue(ev.target.dataItem.get("value"));
+      polygonSeries.zoomToDataItem(ev.target.dataItem);
+      polygonSeries.hide();
+      polygonSeries.show();
     });
   
   
